e2e9a5b0705239082facebd8f383c29b6a8f493b
blackefr <repak120@inbox.ru>
2021-06-22 19:10:14 +0300

    more short

--- sec/js/seconds.js
+++ sec/js/seconds.js
@@ -1,23 +1,17 @@
 const masterElem = document.querySelector(".mainstore");
 let mili = 0;
 let timer;
-
+function converter(data) {
+  return String(data).slice(0, 2) < 10
+    ? "0" + String(data).slice(0, 2)
+    : String(data).slice(0, 2);
+}
 function start() {
   clearInterval(timer);
   timer = setInterval(() => {
     mili += 10;
     const time = new Date(mili);
-    masterElem.textContent = `${
-      (time.getUTCHours() < 10
-        ? "0" + time.getUTCHours()
-        : time.getUTCHours()) +
-      ":" +
-      (time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes()) +
-      ":" +
-      (time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds()) +
-      ":" +
-      String(time.getMilliseconds()).slice(0, 2)
-    }`;
+    masterElem.textContent = `${converter(time.getUTCHours())}:${converter(time.getMinutes())}:${converter(time.getSeconds())}:${converter(time.getMilliseconds())}`;
   }, 10);
 }
 function pause() {
@@ -29,13 +23,9 @@ function reset() {
   masterElem.textContent = "00:00:00:00";
   document.querySelector(".ulRoot").innerHTML = "";
 }
-
 function toList() {
-  document.querySelector(".ulRoot").innerHTML =
-    document.querySelector(".ulRoot").innerHTML +
-    `<li>${masterElem.textContent}</li>`;
+  document.querySelector(".ulRoot").innerHTML +=  `<li>${masterElem.textContent} </li>`;
 }
-
 document.querySelector(".st").addEventListener("click", () => start());
 document.querySelector(".sp").addEventListener("click", () => pause());
 document.querySelector(".res").addEventListener("click", () => reset());
