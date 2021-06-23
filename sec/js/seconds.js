f52ad6f871f0e64b00a4b9cf235f7798c14c04ff
blackefr <repak120@inbox.ru>
2021-06-24 02:07:51 +0300

    complited

--- sec/js/seconds.js
+++ sec/js/seconds.js
@@ -1,30 +1,42 @@
-const masterElem = document.querySelector(".mainstore");
+import {circleBtn, masterElem, pauseBtn, resetBtn, startBtn, rootUl} from "./storage/storage.js";
 let mili = 0;
-let timer;
+let timer, forLaps;
 function converter(data) {
-  return String(data).slice(0, 2) < 10
-    ? "0" + String(data).slice(0, 2)
-    : String(data).slice(0, 2);
+  return String(data).slice(0, 3) < 10
+    ? "0" + String(data).slice(0, 3)
+    : String(data).slice(0, 3);
 }
 function start() {
   clearInterval(timer);
+  pauseBtn.disabled = circleBtn.disabled = false;
+  resetBtn.disabled = startBtn.disabled = true;
   timer = setInterval(() => {
     mili += 10;
     const time = new Date(mili);
-    masterElem.textContent = `${converter(time.getUTCHours())}:${converter(time.getMinutes())}:${converter(time.getSeconds())}:${converter(time.getMilliseconds())}`;
+    forLaps =
+      time.getMilliseconds() < 100
+        ? "0" + time.getMilliseconds()
+        : time.getMilliseconds();
+    masterElem.textContent = `${converter(time.getUTCHours())}:${converter(
+      time.getMinutes()
+    )}:${converter(time.getSeconds())}:${converter(
+      time.getMilliseconds()
+    ).slice(0, 2)}`;
   }, 10);
 }
 function pause() {
   clearInterval(timer);
+  resetBtn.disabled = startBtn.disabled = false;
+  pauseBtn.disabled = circleBtn.disabled = true;
 }
 function reset() {
   clearInterval(timer);
   mili = 0;
   masterElem.textContent = "00:00:00:00";
-  document.querySelector(".ulRoot").innerHTML = "";
+  rootUl.innerHTML = "";
 }
 function toList() {
-  document.querySelector(".ulRoot").innerHTML +=  `<li>${masterElem.textContent} </li>`;
+  rootUl.innerHTML = `<li> ${rootUl.children.length + 1}) ${masterElem.textContent.slice(0, 9)}${forLaps} </li>` + rootUl.innerHTML;
 }
 document.querySelector(".startbtn").addEventListener("click", () => start());
 document.querySelector(".pausebtn").addEventListener("click", () => pause());
