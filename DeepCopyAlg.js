b930cdc60bcd4298079ad8276225b6cfbc17df71
blackefr <repak120@inbox.ru>
2021-07-08 21:49:50 +0300

    repair

--- DeepCopyAlg.js
+++ DeepCopyAlg.js
@@ -1,41 +1,34 @@
-
 // пример для реализации алгоритма
 const obj = {
-  gogi: null,
-  gogu: undefined,
+  someNull: null,
+  someUndefined: undefined,
   name: "John",
   arr: [1, 2, 3, 4, { a: 3, b: 4, arrt: [{ t: 3 }, 1, 23, 4] }, 5, 6],
   ot: {
     u: 2,
     y: 3,
   },
-  logi: function () {
+  returnSomeNumber: function () {
     return 42;
   },
   data: new Date(),
 };
-// конец примера 
+// конец примера
 //--------------------------------------------------------------------------
-
 
 // функция клолнирования
 function superDeep(obj) {
   const clone = {};
   //здесь важно понимать, с чем мы все таки работаем и писать условия исходя из получаемых данных, уверен, будут вопросы по new Date() )))
-  if (
-    (Array.isArray(obj) == false && typeof obj !== "object") ||
-    typeof obj === "function" ||
-    obj instanceof Date || obj === null
-  ) {
+  if (obj instanceof Date) {
+    return new Date(obj);
+  } else if (obj === null || (!Array.isArray(obj) && typeof obj !== "object")) {
     return obj;
   }
-
   if (Array.isArray(obj) == true) {
     return obj.map((i) => superDeep(i));
   } else if (typeof obj == "object") {
-    for (const key in obj) {
-      clone[`${key}`] = superDeep(obj[`${key}`]);
-    }
+    Object.keys(obj).forEach((i) => (clone[i] = superDeep(obj[i])));
   }
   return clone;
 }
@@ -44,35 +37,45 @@ function superDeep(obj) {
 const clone = superDeep(obj);
 //экземпляр -------------------------------------------
 
-
-
-// функция проверки копирования 
+// функция проверки копирования
 function toEqw(natural, clone, storage = []) {
   const arrayu = storage;
   if (natural !== clone) {
     // работа исключительно с данными ссылочного типа. непримитивы сравниваются по ссылочному значению, поэтому,
     // если они повторяют друг друга, то они равны, если же они скопированы, то они не будут равны и запушатся в массив с соотв записью)
-    if (Array.isArray(clone) === true) {
-      arrayu.push(`array copy ${JSON.stringify(clone)}`);
-    } else if (typeof clone === "object") {
-      if (clone === null) {
-        arrayu.push(`obj copy 123`);
-      } else {
-        arrayu.push(`obj copy ${JSON.stringify(clone)}`);
-      }
+    if (Array.isArray(clone)) {
+      arrayu.push(
+        `${JSON.stringify(natural)} не равно ${JSON.stringify(clone)}`
+      );
+    } else if (typeof clone === "object" && !(clone instanceof Date)) {
+      arrayu.push(
+        `${JSON.stringify(natural)} не равно ${JSON.stringify(clone)}`
+      );
+    }
+    if (clone instanceof Date) {
+      arrayu.push(`${natural} не равно ${clone} `);
+    }
+    // ФУНКЦИИ: если сравнивать просто 2 объекта, то функции совершенно различны
+    if (typeof clone === "function") {
+      arrayu.push(`${clone} не равно ${clone}`);
     }
     // примитивы сравниваются по значению, а это означает, что они в любом случае будут равны.
-  } else if (natural === clone) {
-    arrayu.push(`copied primitive ${clone}`);
+  } else {
+    // ФУНКЦИИ: если сравнивать obj и итог функции глубокого копирования, то они будут равны (причуда js?)
+    if (typeof clone === "function") {
+      arrayu.push(`${clone} не равно ${clone}`);
+    } else {
+      arrayu.push(`${natural} не равно ${clone}`);
+    }
   }
 
-  if (Array.isArray(natural) === true && Array.isArray(clone) === true) {
+  if (Array.isArray(natural) && Array.isArray(clone)) {
     return natural.map((item, index) => {
       toEqw(item, clone[index], arrayu);
     });
   } else if (typeof natural == "object" && typeof clone == "object") {
     for (const key in natural) {
-      toEqw(natural[`${key}`], clone[`${key}`], arrayu);
+      toEqw(natural[key], clone[key], arrayu);
     }
   }
 
@@ -80,5 +83,25 @@ function toEqw(natural, clone, storage = []) {
 }
 // функция проверки копирования --------------------------------------------------
 
+const obj2 = {
+  someNull: null,
+  someUndefined: undefined,
+  name: "John",
+  arr: [1, 2, 3, 4, { a: 3, b: 4, arrt: [{ t: 3 }, 1, 23, 4] }, 5, 6],
+  ot: {
+    u: 2,
+    y: 3,
+  },
+  returnSomeNumber: function () {
+    return 42;
+  },
+  data: new Date(),
+};
+
+obj.logi = function logi() {
+  console.log("sasaycudassay");
+};
+
 console.log(toEqw(obj, clone));
-console.log(clone, obj)
+console.log(clone, obj);
+console.log(toEqw(obj, obj2));
